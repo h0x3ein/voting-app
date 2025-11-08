39cd88dab4678d01a2d4d6dadc0fec9fbdc896c4
Hossein Jafari <Hosseinjafari3264@gmail.com>
2025-11-08 14:16:57 +0330

    refactore: add README.md

--- gcp/terraform-bootstrap/outputs.tf
+++ gcp/terraform-bootstrap/outputs.tf
@@ -2,8 +2,3 @@ output "tf_bucket_name" {
   description = "The name of the Terraform state bucket."
   value       = google_storage_bucket.tf_bucket.name
 }
-
-
-output "tf_bucket_name" {
-  value = google_storage_bucket.tf_bucket.name
-}
