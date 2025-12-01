e43e42c8f9afd31c07f46a0a34c0a4fb5fc2c23f
Hossein Jafari <Hosseinjafari3264@gmail.com>
2025-12-01 13:28:44 +0330

    feat(gke): provision simple gke cluster using terraform

--- gke/gke-terraform/main.tf
+++ gke/gke-terraform/main.tf
@@ -1,3 +1,10 @@
 module "api" {
   source = "./modules/api"
 }
+
+
+module "gke" {
+  source = "./modules/gke"
+  region = var.region
+  depends_on = [ module.api ]
+}
---
+++ gke/gke-terraform/modules/gke/main.tf
@@ -0,0 +1,5 @@
+resource "google_container_cluster" "hs_gke_cluster" {
+  name = "test-cluster"
+  location = var.region
+  initial_node_count = 1
+}
