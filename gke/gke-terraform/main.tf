ecbc3c5e4c71efa0722cae36361f99472a121410
Hossein Jafari <Hosseinjafari3264@gmail.com>
2025-12-04 16:00:36 +0330

    refactore(api module): change name api module into project

--- gke/gke-terraform/main.tf
+++ gke/gke-terraform/main.tf
@@ -1,10 +1,10 @@
-module "api" {
-  source = "./modules/api"
+module "project" {
+  source = "./modules/project"
 }
 
 
 module "gke" {
   source = "./modules/gke"
   region = var.region
-  depends_on = [ module.api ]
+  depends_on = [ module.project ]
 }
